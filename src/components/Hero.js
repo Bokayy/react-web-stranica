4c3a0e9f8130f4de12a949a2c1cbd33aef744bbd
Bokayy <boris.milojevic13@gmail.com>
2022-02-25 12:52:11 +0100

    moram doraditi

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button } from './Button'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import './Hero.css'
+
+
+function Hero() {
+  return (
+    <div>
+       <video src="/videos/video.mp4" autoPlay loop muted></video>
+       <h1>Avantura počinje</h1>
+       <p>Nemojte čekati, bukirajte danas, mkaaay</p>
+       <div className="hero-btns">
+           <Button className = "btns" buttonStyle="btn-outline" buttonSize="btn-large">
+               REZERVIRAJTE SAD!
+           </Button>
+           <Button className = "btns" buttonSize="btn-large">POGLEDAJ PONUDU <FontAwesomeIcon Icon={faGlobe}/></Button>
+       </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHotel } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { Button } from './Button';
+import "./NavBar.css"
 
 function NavBar() {
   const [click, setClick] = useState(false);
   const clickHandler = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const [button,setButton] = useState(true);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+
+  useEffect(() =>{showButton();},[])
+
+  window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={clickHandler}>
             Booking <FontAwesomeIcon icon={faHotel} />
           </Link>
           <div className="menu-icon" onClick={clickHandler}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
+          <ul className = {click ? "nav-menu active" : "nav-menu"}>
+            <li className='nav-item'>
+              <Link to="/" className = "nav-links" onClick={closeMobileMenu}>Početna</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/" className = "nav-links" onClick={closeMobileMenu}>Servisi</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/" className = "nav-links" onClick={closeMobileMenu}>Proizvodi</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="/" className = "nav-links" onClick={closeMobileMenu}>Kontakt</Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn-outline'>Kontakt</Button>}
         </div>
       </nav>
     </>
